6d3bb23cf6659445762eac1974b8cabf6bae1dd4
Ted Brown <tedbrownxr@gmail.com>
2024-09-16 16:43:55 -0700

    Initial commit of unity project

---
+++ Assets/DevKit/Pointer/CanvasPointerCheck.cs
@@ -0,0 +1,44 @@
+﻿// Source: https://answers.unity.com/questions/1539189/check-if-mouse-is-over-ui.html
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DvKit
+{
+	[RequireComponent(typeof(EventTrigger))]
+	public class CanvasPointerCheck : MonoBehaviour
+	{
+		public static bool IsPointerOnCanvas => s_hoveredCanvasCount > 0;
+
+		private static int s_hoveredCanvasCount;
+
+		private void HandleHoverCanvas()
+		{
+			s_hoveredCanvasCount++;
+		}
+		private void HandleUnhoverCanvas()
+		{
+			s_hoveredCanvasCount = Mathf.Max(s_hoveredCanvasCount - 1, 0);
+		}
+
+		protected void Start()
+		{
+			EventTrigger eventTrigger = GetComponent<EventTrigger>();
+
+			if (eventTrigger != null)
+			{
+				EventTrigger.Entry enterUIEntry = new EventTrigger.Entry();
+				// Pointer Enter
+				enterUIEntry.eventID = EventTriggerType.PointerEnter;
+				enterUIEntry.callback.AddListener((eventData) => { HandleHoverCanvas(); });
+				eventTrigger.triggers.Add(enterUIEntry);
+
+				//Pointer Exit
+				EventTrigger.Entry exitUIEntry = new EventTrigger.Entry();
+				exitUIEntry.eventID = EventTriggerType.PointerExit;
+				exitUIEntry.callback.AddListener((eventData) => { HandleUnhoverCanvas(); });
+				eventTrigger.triggers.Add(exitUIEntry);
+			}
+		}
+	}
+}
---
+++ Assets/DevKit/Pointer/Pointer.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace DevKit
+{
+	// NOTE: To get BlockedByCanvas to work, add CanvasPointerCheck to all Canvas elements in your scene.
+	public class Pointer : MonoBehaviour 
+	{
+		public bool HasTarget => _pointerTarget != null;
+		public PointerTarget Target => _pointerTarget;
+		public Quaternion Rotation => Quaternion.LookRotation(GetRay().direction, Vector3.up);
+		public Vector3 HitNormal => _hitNormal;
+		public Vector3 HitPoint => _hitPoint;
+		public Vector3 Origin => GetRay().origin;
+
+		[SerializeField] protected LayerMask _layerMask = -1;
+
+		protected int _lastUpdatedFrame;
+		protected PointerTarget _pointerTarget;
+		protected Ray _ray;
+		protected Vector3 _hitNormal;
+		protected Vector3 _hitPoint;
+
+		protected virtual Ray GetRay ()
+		{
+			if (Time.frameCount == _lastUpdatedFrame)
+			{
+				return _ray;
+			}
+			_lastUpdatedFrame = Time.frameCount;
+			_ray = new Ray(transform.position, transform.forward);
+			return _ray;
+		}
+
+		protected virtual PointerTarget GetPointerTarget ()
+		{
+			Ray ray = GetRay();
+			if (Physics.Raycast(ray, out RaycastHit hit, 1000, _layerMask, QueryTriggerInteraction.Ignore))
+			{
+				_hitPoint = hit.point;
+				_hitPoint = hit.normal;
+				PointerTarget target = hit.collider.gameObject.GetComponentInParent<PointerTarget>();
+				if (target.IsActive)
+				{
+					return target;
+				}
+			}
+			return null; 
+		}
+
+		protected virtual void Update ()
+		{
+			PointerTarget previousTarget = _pointerTarget;
+			PointerTarget currentTarget = GetPointerTarget();
+
+			// if the current target is the same as the previous target, only do something if the target was disabled
+			if (currentTarget == previousTarget)
+			{
+				if (currentTarget != null)
+				{
+					if (currentTarget.IsActive == false)
+					{
+						currentTarget.Unhover();
+						currentTarget = null;
+					}
+				}
+			}
+			// If we have a new target, unhover the old and hover the new
+			else if (currentTarget != previousTarget)
+			{
+				if (previousTarget != null)
+				{
+					previousTarget.Unhover();
+				}
+				// Here we assume GetPointerTarget checks if the target is enabled or not
+				// before it returns one. Otherwise, it should return a null value.
+				if (currentTarget != null)
+				{
+					currentTarget.Hover();
+				}
+			}
+
+			_pointerTarget = currentTarget;
+		}
+	}
+}
